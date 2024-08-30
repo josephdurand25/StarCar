fb724e0f3cdd7081ed8d274e6265ced93a7f1606
josephdurand25 <durandjosephadji25@gmail.com>
2024-08-30 20:25:19 +0100

    J'ai moodifier ce fichier pour qu'il puisse servir remixicons , je l'ai aussi adapter en fonction de mes routes

--- app.js
+++ app.js
@@ -8,16 +8,13 @@ var expressLayouts = require('express-ejs-layouts'); // Ajouter l'importation
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testsRouter = require('./routes/test');
+var carsRouter = require('./routes/cars');
+var driversRouter = require('./routes/drivers');
+var pannesRouter = require('./routes/pannes');
+var versementsRouter = require('./routes/versements');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Configurer express-ejs-layouts
-app.use(expressLayouts); 
-app.set('layout', 'layouts/app');
 
 
 app.use(logger('dev'));
@@ -25,9 +22,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+app.use(expressLayouts); 
+app.set('views', path.join(__dirname, 'views'));
+// view engine setup
+app.set('view engine', 'ejs');
+// Configurer express-ejs-layouts
+app.set('layout', './layouts/squelette');
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testsRouter);
+app.use('/cars', carsRouter);
+app.use('/drivers', driversRouter);
+app.use('/pannes', pannesRouter);
+app.use('/versements', versementsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
